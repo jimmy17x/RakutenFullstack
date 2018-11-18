a8d4040c21b090bd56c413d0502a175a165b7edf
Divyanshu <divyanshu@freecharge.com>
2018-11-18 20:25:18 +0530

    Created basic APIs to upload files

--- src/main/java/com/rakuten/fullstackrecruitmenttest/FullStackRecruitmentTestApplication.java
+++ src/main/java/com/rakuten/fullstackrecruitmenttest/FullStackRecruitmentTestApplication.java
@@ -2,8 +2,12 @@ package com.rakuten.fullstackrecruitmenttest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.rakuten.fullstackrecruitmenttest.config.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class FullStackRecruitmentTestApplication {
 
 	public static void main(String[] args) {
