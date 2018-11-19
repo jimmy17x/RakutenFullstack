b2c14cf3293a7380e122cdedf4bf28d95225a162
Divyanshu <divyanshu@freecharge.com>
2018-11-19 20:06:07 +0530

    m

--- fullstackrecruitmenttest/src/App.js
+++ fullstackrecruitmenttest/src/App.js
@@ -5,9 +5,45 @@ import { FilePond,registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+import Reactable from 'reactable';  
+import axios from 'axios';
+
 registerPlugin(FilePondPluginImagePreview);
 
-class App extends Component {
+
+var Table = Reactable.Table,  
+Thead = Reactable.Thead,
+Th = Reactable.Th;
+
+class App extends Component { 
+
+	constructor(props){
+	   super(props);
+	      this.state = {
+	    		  allEmpl: []
+	      }
+	}
+	      
+	 renderTable() {
+		   return (
+				   <Table className="table"
+					   noDataText="No matching records found"
+				        itemsPerPage={5}
+				        currentPage={0}
+				        sortable={true}
+				        data={this.state.allEmpl}>
+				        <Thead>
+				          <Th column="emplId">ID</Th>
+				          <Th column="name">NAME</Th>
+				          <Th column="dept">DEPARTMENT</Th>
+				          <Th column="designation">DESIGNATION</Th>
+				          <Th column="salary">SALARY</Th>
+				          <Th column="joingingDate">JOINING DATE</Th>
+				        </Thead>
+				      </Table>
+		   )
+		 }
+	
   render() {
     return (
     		<div className="app">
@@ -16,12 +52,20 @@ class App extends Component {
     		  </header>
     		  <FilePond  allowMultiple={true} server="http://localhost:8080/uploadMultipleEmployee"/>
     			<form method = "get" action="http://localhost:8080/downloadEmployeeRecord/AllEmployeeRecords.csv">
-    			<input type="submit" value="Get All Employee"/>
+    			<input className = "get-report" type="submit" value="Download All Employee report"/>
     		  </form>
+    		   {this.renderTable()}
     		</div>
+    		
     		  
     );
   }
+  
+  componentDidMount() {
+	 
+		  axios.get(` http://localhost:8080/allemployees`)
+		    .then((response) => this.setState({allEmpl: response.data}));
+	}
 }
 
 export default App;
