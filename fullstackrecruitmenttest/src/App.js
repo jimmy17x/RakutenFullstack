66499054ee5ab7561e356bcab0d3158bb9f9d1b7
Divyanshu <divyanshu@freecharge.com>
2018-11-19 18:11:57 +0530

    Added UI for uploading record and fetching all record

--- fullstackrecruitmenttest/src/App.js
+++ fullstackrecruitmenttest/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import Main from './container/Main';
 import './App.css';
+import { FilePond,registerPlugin } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+registerPlugin(FilePondPluginImagePreview);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+    		<div className="app">
+    		 <header className="app-header">
+    		   <h1 className="app-title">Upload Employee Record(s)</h1>
+    		  </header>
+    		  <FilePond  allowMultiple={true} server="http://localhost:8080/uploadMultipleEmployee"/>
+    			<form method = "get" action="http://localhost:8080/downloadEmployeeRecord/AllEmployeeRecords.csv">
+    			<input type="submit" value="Get All Employee"/>
+    		  </form>
+    		</div>
+    		  
     );
   }
 }
